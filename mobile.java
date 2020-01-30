e84dfb6a2e6067be5cd3cb0345e20733938642dc
Rishav7 <rajrishav381.rr@gmail.com>
2020-01-30 19:51:49 +0530

    we

--- mobile.java
+++ mobile.java
@@ -1,5 +1,7 @@
 Class Java {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Sheshank");
+        System.out.println("Andhra udar nahi hai ");
         }
 }
